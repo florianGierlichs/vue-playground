607eb28562d4cf8f5d8b9825564b35598782e581
florian_gierlichs <gierlichs@d-sire.com>
2020-09-10 16:54:14 +0200

    Add Header

---
+++ src/components/Header.vue
@@ -0,0 +1,43 @@
+<template>
+  <header>
+    <div>Logo</div>
+    <nav>
+      <ul>
+        <li>home</li>
+        <li>about</li>
+        <li>contact</li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    logo: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+header {
+  display: flex;
+  background-color: #333;
+  color: white;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  > * {
+    padding: 0 50px;
+  }
+}
+
+ul {
+  display: flex;
+  list-style: none;
+  > li {
+    margin-right: 10px;
+  }
+}
+</style>
