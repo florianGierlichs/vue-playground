1d771ae769a03a07c1e7255164b0da997b55af88
florianGierlichs <gierlichs@d-sire.com>
2020-09-11 11:52:56 +0200

    Add Cards with prop drilling

---
+++ src/components/Cards.vue
@@ -0,0 +1,31 @@
+<template>
+  <ul>
+    <Card v-for="user in users" v-bind:key="user.name" v-bind:user="user" />
+  </ul>
+</template>
+
+<script>
+import Card from './Card';
+
+export default {
+  name: 'Cards',
+  components: {
+    Card,
+  },
+  props: {
+    users: {
+      type: Array,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+ul {
+  margin: 0 auto;
+  padding: 0;
+  list-style-type: none;
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
